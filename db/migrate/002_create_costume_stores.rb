16c90adfc383ba101892e56b47ed197f949e316e
Sean Altemose <sean@seanaltemose.com>
2018-06-25 20:37:03 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -9,7 +9,7 @@ def change
     t.integer :num_of_employees
     t.boolean :still_in_business
     t.datetime :opening_time
-    t.datetime :end_time
+    t.datetime :closing_time
     t.timestamps
     end
   end
